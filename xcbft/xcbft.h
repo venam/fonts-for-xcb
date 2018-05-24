b077785043d7b2e504304709060c9a71f1ac43d6
Patrick Louis <patrick@psychology.wtf>
2018-05-24 12:14:05 +0300

    If Xrm not available use highest dpi
    In case the dpi is not found in any resources we fallback to checking
    the highest dpi of any screen

--- xcbft/xcbft.h
+++ xcbft/xcbft.h
@@ -619,7 +619,9 @@ xcbft_get_dpi(xcb_connection_t *c)
 {
 	int i;
 	long dpi;
+	long xres;
 	xcb_xrm_database_t *xrm_db;
+	xcb_screen_iterator_t iter;
 
 	i = 0;
 	// default to something common just in case everything else fails
@@ -629,14 +631,45 @@ xcbft_get_dpi(xcb_connection_t *c)
 		i = xcb_xrm_resource_get_long(xrm_db, "Xft.dpi", NULL, &dpi);
 		if (i < 0) {
 			fprintf(stderr,
-				"Could not fetch value of Xft.dpi from Xresources falling back to lowest dpi found");
+				"Could not fetch value of Xft.dpi from Xresources falling back to highest dpi found");
 		}
 		xcb_xrm_database_free(xrm_db);
-	}
-	if ( xrm_db == NULL || i < 0) {
+	} else {
 		fprintf(stderr,
-			"Could not open Xresources database falling back to lowest dpi found");
-		// TODO loop through the dpi from the screens
+			"Could not open Xresources database falling back to highest dpi found");
+	}
+
+	if ( xrm_db == NULL || i < 0) {
+		iter = xcb_setup_roots_iterator(xcb_get_setup(c));
+		dpi = 0;
+		for (; iter.rem; xcb_screen_next(&iter)) {
+			/*
+			* Inspired by xdpyinfo
+			*
+			* there are 2.54 centimeters to an inch; so
+			* there are 25.4 millimeters.
+			*
+			* dpi = N pixels / (M millimeters / (25.4 millimeters / 1 inch))
+			*     = N pixels / (M inch / 25.4)
+			*     = N * 25.4 pixels / M inch
+			*/
+			if (iter.data != NULL) {
+				xres = ((((double) iter.data->width_in_pixels) * 25.4) /
+					((double) iter.data->width_in_millimeters));
+
+				// ignore y resolution for now
+				//yres = ((((double) iter.data->height_in_pixels) * 25.4) /
+				//	((double) iter.data->height_in_millimeters));
+				if (xres > dpi) {
+					dpi = xres;
+				}
+			}
+		}
+
+		if (dpi == 0) {
+			// if everything fails use 96
+			dpi = 96;
+		}
 	}
 
 	return dpi;
