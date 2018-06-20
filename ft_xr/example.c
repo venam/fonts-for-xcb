3d85f3b1485e98538a5238f3068a179e57091e53
Patrick Louis <patrick@psychology.wtf>
2018-06-20 12:22:58 +0300

    - add more debugging in example
    Added squares to debug display the limit we can put around the text
    using only the pixel size.
    It would be interesting to have a function automatically drawing inside
    a pixmap of this size the text required (a sort of wrapper)

--- ft_xr/example.c
+++ ft_xr/example.c
@@ -137,12 +137,26 @@ main(int argc, char **argv)
 	rectangles[0].y = 60+pix_size*0.2;
 	xcb_poly_fill_rectangle(c, pmap, gc, 1, rectangles);
 
-	// bottom right
+	// bottom right using pix_size
+	rectangles[0].x = 50+(pix_size*text.length/2.0)+pix_size*0.4;
+	rectangles[0].y = 60+pix_size*0.2;
+	xcb_poly_fill_rectangle(c, pmap, gc, 1, rectangles);
+
+	// top right using pix_size
+	rectangles[0].x = 50+(pix_size*text.length/2.0)+pix_size*0.4;
+	rectangles[0].y = 60-pix_size-pix_size*0.2;
+	xcb_poly_fill_rectangle(c, pmap, gc, 1, rectangles);
+
+	values[0] = 0x0000FF | 0xff000000;
+	values[1] = 0;
+	xcb_change_gc(c, gc, mask, values);
+
+	// bottom right using advance (blue)
 	rectangles[0].x = 50+advance.x+pix_size*0.2;
 	rectangles[0].y = 60+pix_size*0.2;
 	xcb_poly_fill_rectangle(c, pmap, gc, 1, rectangles);
 
-	// top right
+	// top right using advance (blue)
 	rectangles[0].x = 50+advance.x+pix_size*0.2;
 	rectangles[0].y = 60-pix_size-pix_size*0.2;
 	xcb_poly_fill_rectangle(c, pmap, gc, 1, rectangles);
